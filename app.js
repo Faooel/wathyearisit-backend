ce308ab63482637aa359ada884973f989737cbc4
Benoit <giannini.benoit@gmail.com>
2023-04-07 17:14:14 +0200

    modif

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var yearRouter = require('./routes/year');
 
 var app = express();
 const cors = require('cors');
@@ -18,5 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/year', yearRouter);
 
 module.exports = app;
